2adbb81334d7567dfbdcb677eeb86c4184a0eef7
JD <poker98@web.de>
2019-09-01 22:33:55 +0200

    added BackgroundTasks

--- GithubStatisticsCore/Services/BackgroundService.cs
+++ GithubStatisticsCore/Services/BackgroundService.cs
@@ -2,18 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GithubStatisticsCore.Models;
+using GithubStatisticsCore.Services.DataService;
+using GithubStatisticsCore.Services.GithubApi;
 
 namespace GithubStatisticsCore.Services
 {
     public interface IBackgroundService
     {
-        void RunDemoTask();
+        void AddNewProjects();
+        void UpdateProjects();
+        void SaveViews();
+        void RemoveAndCalculateTotalViews();
     }
+
     public class BackgroundService : IBackgroundService
     {
-        public void RunDemoTask()
+        private readonly IGithubDataService _githubDataService;
+        private readonly IGithubApiRepoProcessor _githubApiRepoProcessor;
+
+        public BackgroundService(IGithubDataService githubDataService, IGithubApiRepoProcessor githubApiRepoProcessor)
         {
-            Console.WriteLine("its me hangfire");
+            _githubDataService = githubDataService;
+            _githubApiRepoProcessor = githubApiRepoProcessor;
+        }
+
+
+        public async void AddNewProjects() //TODO async vs async task
+        {
+            List<GithubProject> githubProjects = await _githubApiRepoProcessor.GetGithubRepoInfo("jdevdain");
+            _githubDataService.SaveGithubProjects(githubProjects);
+        }
+
+        public async void UpdateProjects()
+        {
+            //TODO change to Database access
+            List<GithubProject> githubProjects = await _githubApiRepoProcessor.GetGithubRepoInfo("jdevdain");
+            _githubDataService.UpdateGithubProjects(githubProjects);
+        }
+
+        public async void SaveViews()
+        {
+            List<GithubProjectView> githubProjectViews = await _githubApiRepoProcessor.GetGithubRepoViews("jdevdain");
+            _githubDataService.SaveViews(githubProjectViews);
+        }
+
+        public void RemoveAndCalculateTotalViews()
+        {
+            _githubDataService.RemoveDuplicatesInViewsAndUpdateTotal();
         }
     }
 }
